74f4959656168b24aca507d683f65c7c545aa0f2
freicaneca <felipeduque@riseup.net>
2015-11-28 20:30:12 -0200

    Organizando diretorios

---
+++ core/pertinencia.m
@@ -0,0 +1,7 @@
+% Calcula a pertinencia de cada dado aos clusters.
+% Recebe: individuo (em forma de matriz) e dados;
+% Retorna: lista com n elementos. Cada elemento i
+% contem os dados pertencentes ao centroide i.
+
+
+
