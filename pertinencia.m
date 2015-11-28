d5731c7f86cf6d22e623f65a40dc1bf7e5346787
freicaneca <felipeduque@riseup.net>
2015-11-28 21:36:58 -0200

    Delete pertinencia.m

--- pertinencia.m
+++
@@ -1,4 +0,0 @@
-% Calcula a pertinencia de cada dado aos clusters.
-% Recebe: individuo (em forma de matriz) e dados;
-% Retorna: lista com n elementos. Cada elemento i eh uma matriz
-% contendo os dados pertencentes ao centroide i.
